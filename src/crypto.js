fe1038559b078e06b9fbdf2844759a1db759e7ad
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-06-08 14:43:50 +0530

    fix: make esbuild think @peculiar/webcrypto is external

--- src/crypto.js
+++ src/crypto.js
@@ -1,7 +1,11 @@
 export const crypto = (() => {
   if (typeof globalThis?.crypto?.subtle === 'object') return globalThis.crypto;
   if (typeof globalThis?.crypto?.webcrypto?.subtle === 'object') return globalThis.crypto.webcrypto;
-  return new ((typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require)(
-    '@peculiar/webcrypto',
-  ).Crypto)();
+  try {
+    return new ((typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require)(
+      '@peculiar/webcrypto',
+    ).Crypto)();
+  } catch (e) {
+    throw e;
+  }
 })();
