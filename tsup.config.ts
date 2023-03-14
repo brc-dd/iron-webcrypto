acb294170bcba39b0224aeac60392226aaf9578d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-03-14 12:51:22 +0530

    chore: fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,3 @@
-import { cp } from 'node:fs/promises'
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
@@ -8,10 +6,4 @@ export default defineConfig({
   dts: true,
   format: ['esm', 'cjs'],
   treeshake: true,
-  onSuccess: async () => {
-    await cp(
-      fileURLToPath(new URL('dist/index.d.ts', import.meta.url)),
-      fileURLToPath(new URL('dist/index.d.cts', import.meta.url))
-    )
-  },
 })
